b27b5e3f181bb77f1aba81b14a0543dc8f6c33a2
Richard Morello <rmorello@liaison-intl.com>
2014-04-14 17:47:23 -0400

    Minor changes---just get the response to work.

--- models/pairbot.rb
+++ models/pairbot.rb
@@ -1,27 +1,33 @@
 require 'cinch'
 require 'pairs'
 
-class PairBot
-  include Cinch::Plugin
-  listen_to :channel
-  match /pair with (.+)/
+# class PairBot
+#   include Cinch::Plugin
+#   listen_to :channel
+#   match /pair with (.+)/
 
-  def initialize(bot)
-    @pairs = Pairs.new
-    @users = {}
-  end
+#   def initialize(bot)
+#     @pairs = Pairs.new
+#     @users = {}
+#   end
 
-  def execute(m, nick)
-    @pairs.join(m.user.nick, nick)
-  end
-end
+#   def execute(m, nick)
+#     @pairs.join(m.user.nick, nick)
+#     m.reply "You are now paired with #{nick}"
+#   end
+# end
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = 'irc.freenode.org'
     c.channels = ['#cinch-bots']
+    c.nick = "pairbot"
     c.verbose = true
-    c.plugins.plugins = [PairBot]
+    # c.plugins.plugins = [PairBot]
+  end
+
+  on :message, /^!pair with (.+)/ do |m, query|
+    m.reply "Pairing with #{query}"
   end
 end
 
--- test/bot_test.rb
+++ test/bot_test.rb
@@ -6,9 +6,13 @@ require 'pairbot'
 
 describe PairBot do
           include Cinch::Test
-          let (:bot) { make_bot(PairBot, {}) }
+          # let (:bot) { make_bot(PairBot, {}) }
 
           it "can create a pair" do
+            bot = make_bot(PairBot, {})
+            puts "=" * 80
+            puts "This is a test message"
+
             msg = make_message(bot, "!pair with abe")
             get_replies(msg)
           end
