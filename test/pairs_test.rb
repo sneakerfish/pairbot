529f827aad859922fb4170a8c1ab452f022db9a4
Richard Morello <richard@sneakerfish.com>
2014-04-13 21:32:53 -0400

    Simplify the test by having a let.

--- test/pairs_test.rb
+++ test/pairs_test.rb
@@ -3,41 +3,39 @@ require 'minitest/autorun'
 require 'pairs'
 
 describe Pairs do
+          let (:pair) { Pairs.new }
+
           it "can be created with no arguments" do
             Pairs.new.must_be_instance_of Pairs
           end
 
           it "can assign a pair" do
-            p = Pairs.new
-            p.joins("a", "b")
-            p.paired?("a").must_equal true
-            p.paired?("b").must_equal true
+            pair.joins("a", "b")
+            pair.paired?("a").must_equal true
+            pair.paired?("b").must_equal true
           end
 
           it "can add to a pair" do 
-            p = Pairs.new
-            p.joins("a", "b")
-            p.joins("b", "c")
-            p.paired?("a").must_equal true
-            p.paired?("b").must_equal true
+            pair.joins("a", "b")
+            pair.joins("b", "c")
+            pair.paired?("a").must_equal true
+            pair.paired?("b").must_equal true
           end
 
           it "can remove a person from a pair" do 
-            p = Pairs.new
-            p.joins("a", "b")
-            p.remove_from_pair("a")
-            p.paired?("a").must_equal false
-            p.paired?("b").must_equal false
+            pair.joins("a", "b")
+            pair.remove_from_pair("a")
+            pair.paired?("a").must_equal false
+            pair.paired?("b").must_equal false
           end
 
           it "can assign two, have one join, have another leave" do
-            p = Pairs.new
-            p.joins("a", "b")  # now a,b
-            p.joins("a", "c")  # now a,b,c
-            p.paired_with("a").must_equal ["b", "c"]
-            p.joins("d", "a")  # now a,d and b,c
-            p.paired_with("a").must_equal ["d"]
-            p.paired_with("b").must_equal ["c"]
+            pair.joins("a", "b")  # now a,b
+            pair.joins("a", "c")  # now a,b,c
+            pair.paired_with("a").must_equal ["b", "c"]
+            pair.joins("d", "a")  # now a,d and b,c
+            pair.paired_with("a").must_equal ["d"]
+            pair.paired_with("b").must_equal ["c"]
           end
  
 end
