c5d431235287eab22e9a4d9c3b96022c96d72832
Richard Morello <richard@sneakerfish.com>
2014-04-13 21:28:33 -0400

    Change the way that joins works; it is directional now.   When you join a pair, it can grow.  If you join a single, you leave the pair you were in.

--- models/pairs.rb
+++ models/pairs.rb
@@ -12,23 +12,29 @@ class Pair
     @pair.member?(name)
   end
 
+  def members
+    @pair.to_a
+  end
+  
   def remove(name)
     @pair.delete(name)
+  end
+
+  def add(name)
+    @pair += [name]
   end
 
   def invalid?
     @pair.size < 2
   end
 end
-    
-
 
 class Pairs
   def initialize
     @pairs = []
     @names = Set.new
   end
-    
+  
   def remove_from_pair(name)
     @pairs.each_with_index do |pair, idx|
             if pair.member?(name)
@@ -42,13 +48,6 @@ class Pairs
     return false
   end
 
-  def make_pair(name1, name2)
-    remove_from_pair(name1)
-    remove_from_pair(name2)
-    @pairs << Pair.new(name1, name2)
-    @names += [name1, name2]
-  end
-
   def paired?(name)
     @pairs.each do |pair|
             if pair.member?(name)
@@ -56,5 +55,37 @@ class Pairs
             end
           end
     return false
+  end
+
+  def paired_with(name)
+    @pairs.each do |pair|
+            if pair.member?(name)
+              return pair.members.select {|i| i != name }
+            end
+          end
+    return []
+  end
+
+  # name2 joins name1
+  def joins(name1, name2)
+    if paired?(name1)
+      find(name1).add(name2)
+    elsif paired?(name2)
+      remove_from_pair(name2)
+      @pairs << Pair.new(name1, name2)
+    else
+      @pairs << Pair.new(name1, name2)
+    end
+    @names += [name1, name2]
+  end
+  
+  private
+  def find(name)
+    @pairs.each do |pair|
+            if pair.member?(name)
+              return pair
+            end
+          end
+    return nil
   end
 end
--- test/pairs_test.rb
+++ test/pairs_test.rb
@@ -9,24 +9,35 @@ describe Pairs do
 
           it "can assign a pair" do
             p = Pairs.new
-            p.make_pair("a", "b")
+            p.joins("a", "b")
             p.paired?("a").must_equal true
             p.paired?("b").must_equal true
           end
 
-          it "can reassign a pair" do 
+          it "can add to a pair" do 
             p = Pairs.new
-            p.make_pair("a", "b")
-            p.make_pair("b", "c")
-            p.paired?("a").must_equal false
+            p.joins("a", "b")
+            p.joins("b", "c")
+            p.paired?("a").must_equal true
             p.paired?("b").must_equal true
           end
 
           it "can remove a person from a pair" do 
             p = Pairs.new
-            p.make_pair("a", "b")
+            p.joins("a", "b")
             p.remove_from_pair("a")
             p.paired?("a").must_equal false
             p.paired?("b").must_equal false
           end
+
+          it "can assign two, have one join, have another leave" do
+            p = Pairs.new
+            p.joins("a", "b")  # now a,b
+            p.joins("a", "c")  # now a,b,c
+            p.paired_with("a").must_equal ["b", "c"]
+            p.joins("d", "a")  # now a,d and b,c
+            p.paired_with("a").must_equal ["d"]
+            p.paired_with("b").must_equal ["c"]
+          end
+ 
 end
