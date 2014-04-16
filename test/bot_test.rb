f164f80b288eb5f818114ec1bf4b36baa016f043
Richard Morello <richard@sneakerfish.com>
2014-04-15 21:41:24 -0400

    Added a test for listing pairs.

--- test/bot_test.rb
+++ test/bot_test.rb
@@ -10,9 +10,27 @@ describe PairBot do
           let (:bot) { make_bot(PairBot, {}) }
 
           it "can create a pair" do
-            msg = make_message(bot, "!pair with abe")
+            msg = make_message(bot, "pair with abe")
             replies = get_replies(msg)
             replies[0].text.must_equal "You are now paired with abe"
           end
 
+          it "can create a pair and assign work." do
+            msg = make_message(bot, "pair with ben")
+            replies = get_replies(msg)
+            replies[0].text.must_equal "You are now paired with ben"
+            msg = make_message(bot, "pair on testing")
+            replies = get_replies(msg)
+            replies[0].text.must_include "now working on testing"
+          end
+
+          it "can list pairs" do 
+            msg = make_message(bot, "pair with charles")
+            replies = get_replies(msg)
+            msg = make_message(bot, "pairs")
+            replies = get_replies(msg)
+            replies.each { |t| puts t.text }
+            replies[0].text.must_include "Pairs are"
+            replies[0].text.must_include "charles"
+          end
 end
