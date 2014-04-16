9103475881dc02a873ec32a18a1a95558f58a57f
Richard Morello <richard@sneakerfish.com>
2014-04-15 21:26:15 -0400

    Added help for pairs.

--- models/pairbot.rb
+++ models/pairbot.rb
@@ -3,9 +3,10 @@ require 'pairs'
 
 class PairBot
   include Cinch::Plugin
-  match /pair with (.+)/, method: :join_pair
-  match /pairs/, method: :query_pairs
-  match /work on (.+)/, method: :work_on
+  match /^pair with (.+)/, method: :join_pair, use_prefix: false
+  match /^pairs/, method: :query_pairs, use_prefix: false
+  match /^pair on (.+)/, method: :work_on, use_prefix: false
+  match /^pair help/, method: :pair_help, use_prefix: false
 
   def initialize(*args)
     super
@@ -30,6 +31,14 @@ class PairBot
                                        (works_on.nil? ? "" : " on " + works_on) + "\n"
                      end
     m.reply message
+  end
+
+  def pair_help(m)
+    m.reply "Commands are:"
+    m.reply "pair with name       -- you pair with this name"
+    m.reply "pair on task         -- your pair works on this task"
+    m.reply "pairs                -- show pairs and what they work on"
+    m.reply "help                 -- this text"
   end
 
   private
